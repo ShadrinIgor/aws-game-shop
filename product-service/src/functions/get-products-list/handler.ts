3f3e490cc8cce59062409620686e2c68c59d28f1
igor.shadrin_rft <F$~@r_9qWL>
2021-04-07 14:51:20 +0500

    Finish working with task-3

--- product-service/src/functions/get-products-list/handler.ts
+++ product-service/src/functions/get-products-list/handler.ts
@@ -4,8 +4,19 @@ import {formatJSONResponse} from '../../libs/apiGateway';
 import {Products} from '../../products/products';
 
 export const getProductsList = async () => {
-    return formatJSONResponse(200, {
-        products: Products
-    });
+    try {
+        return formatJSONResponse(200, {
+            products: Products
+        });
+    } catch (e) {
+        return {
+            statusCode: 500,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
+            },
+            body: JSON.stringify({message: 'Error happened'}),
+        };
+    }
 }
 
