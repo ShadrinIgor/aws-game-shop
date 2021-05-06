491a4545df7f7a301a6046d7520eedbd4db012f0
igor.shadrin_rft <F$~@r_9qWL>
2021-05-06 10:26:06 +0500

    Revert "Task5 TASK 5.1 - DONE TASK 5.2"
    This reverts commit b8cb0d3c

--- import-service/src/functions/import-file-parser/handler.ts
+++ import-service/src/functions/import-file-parser/handler.ts
@@ -16,7 +16,6 @@ const PARSED_FOLDER = 'parsed';
 
 // const dbService = new DbService();
 const BUCKET = 'game-store-uploaded';
-const ERROR_HAPPENED = 'ERROR_HAPPENED';
 
 export const importFileParser = async (event) => {
     console.log('importProductsFile');
@@ -34,37 +33,31 @@ export const importFileParser = async (event) => {
  */
 async function parseFiles(event) {
     console.log('parseFiles');
-    try {
-        for (const record of event.Records) {
-            const recordKey = record.s3.object.key;
-            console.log('parseFiles.file', recordKey);
-            const s3Object = await s3.getObject({
-                Bucket: BUCKET,
-                Key: recordKey
-            }).promise();
+    for (const record of event.Records) {
+        const recordKey = record.s3.object.key;
+        console.log('parseFiles.file', recordKey);
+        const s3Object = await s3.getObject({
+            Bucket: BUCKET,
+            Key: recordKey
+        }).promise();
 
-            const objectStream = new stream.Readable();
-            objectStream._read = () => {
-            };
-            objectStream.push((s3Object as S3.Types.GetObjectOutput).Body);
+        const objectStream = new stream.Readable();
+        objectStream._read = () => {};
+        objectStream.push((s3Object as S3.Types.GetObjectOutput).Body);
 
-            objectStream
-                .pipe(csv())
-                .on('data', async function (data) {
-                        this.pause();
-                        console.log('stream.data', data);
-                        this.resume();
-                    }
-                )
-                .on('end', async function () {
-                        this.pause();
-                        console.log('stream.end');
-                    }
-                )
-
-        }
-    } catch (e) {
-        return formatJSONResponse(422, ERROR_HAPPENED)
+        objectStream
+            .pipe(csv())
+            .on('data', async function (data) {
+                    this.pause();
+                    console.log('stream.data', data);
+                    this.resume();
+                }
+            )
+            .on('end', async function () {
+                    this.pause();
+                    console.log('stream.end');
+                }
+            )
     }
 }
 
@@ -73,10 +66,10 @@ async function parseFiles(event) {
  * @param event
  */
 function moveFilesToParsed(event) {
-    const objectsCount = event.Records.length;
-    let deletedCount = 0;
+    const objectsCount =  event.Records.length;
+    let deletedCount =  0;
 
-    return new Promise(async (resolve) => {
+    return new Promise(async (resolve) =>  {
         for (const record of event.Records) {
             const parsedObjectKey = record.s3.object.key.replace(UPLOAD_FOLDER, PARSED_FOLDER);
             console.log('moveIntoToParsed', record.s3.object.key, ' - ', record.s3.object.key.replace(UPLOAD_FOLDER, PARSED_FOLDER));
@@ -87,20 +80,20 @@ function moveFilesToParsed(event) {
             }).promise();
 
             console.log('deleteObject', record.s3.object.key);
-            s3.deleteObject({
+           s3.deleteObject({
                 Bucket: BUCKET,
                 Key: record.s3.object.key,
             }, (err, data) => {
-                deletedCount++;
+               deletedCount ++;
 
-                if (err) {
-                    console.log('deleteObject.error', err, err.stack);
-                }
-                console.log('deleteObject.success', deletedCount, ' from ', objectsCount, ';', data);
-                if (deletedCount === objectsCount) {
-                    resolve(null);
-                }
-            });
+               if (err) {
+                   console.log('deleteObject.error', err, err.stack);
+               }
+               console.log('deleteObject.success', deletedCount, ' from ', objectsCount, ';', data);
+               if (deletedCount === objectsCount) {
+                   resolve(null);
+               }
+           });
         }
     });
 
--- import-service/src/functions/import-products-file/handler.ts
+++ import-service/src/functions/import-products-file/handler.ts
@@ -3,45 +3,27 @@ import * as AWS from 'aws-sdk';
 import 'source-map-support/register';
 import {formatJSONResponse} from "../../libs/apiGateway";
 
+// const dbService = new DbService();
 const BUCKET = 'game-store-uploaded';
-const INCORRECT_DATA = 'INCORRECT_DATA';
-const ERROR_HAPPENED = 'ERROR_HAPPENED';
-const APPROVED_EXTENSIONS = ['csv'];
 
 export const importProductsFile = async (event) => {
     const fileName = event?.queryStringParameters?.name;
+    console.log('importProductsFile', fileName);
+    const s3 = new AWS.S3({
+        region: 'us-east-1',
+        signatureVersion: 'v4'
+    });
 
-    if(!fileName) {
-        return formatJSONResponse(422, { error: INCORRECT_DATA})
-    }
+    const params = {
+        Bucket: BUCKET,
+        Key: `uploaded/${fileName}`,
+        Expires: 60,
+        ContentType: 'text/csv',
+        ACL: 'public-read'
+    };
 
-    const fileParams = fileName.split('.');
-    const lastParam = fileParams[fileParams.length-1];
-    console.log('fileParams', fileParams, !APPROVED_EXTENSIONS.includes(lastParam));
-    if (!fileParams || fileParams.length === 1 || !APPROVED_EXTENSIONS.includes(lastParam)) {
-        return formatJSONResponse(422, { error: INCORRECT_DATA})
-    }
+    const url = await s3.getSignedUrlPromise('putObject', params);
 
-    try {
-        console.log('importProductsFile', fileName);
-        const s3 = new AWS.S3({
-            region: 'us-east-1',
-            signatureVersion: 'v4'
-        });
-
-        const params = {
-            Bucket: BUCKET,
-            Key: `uploaded/${fileName}`,
-            Expires: 60,
-            ContentType: 'text/csv',
-            ACL: 'public-read'
-        };
-
-        const url = await s3.getSignedUrlPromise('putObject', params);
-
-        return formatJSONResponse(200, url)
-    } catch (e) {
-        return formatJSONResponse(422, ERROR_HAPPENED)
-    }
+    return formatJSONResponse(200, url)
 }
 
